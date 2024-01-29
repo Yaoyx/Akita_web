88118802d64332966f6a8db3e34f185e71a962c3
Yaoyx <yxiaoy@umich.edu>
2024-01-29 01:05:36 -0800

    pass

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,56 @@
-import { Box, Flex, FormControl, FormLabel, Input, Button, Select, VStack, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, FormControl, FormLabel, Input, Button, Select, VStack, Heading, Text, Spinner, Image } from "@chakra-ui/react";
 import { FaUpload } from "react-icons/fa";
+import Plot from "react-plotly.js";
+import React from 'react';
+
 
 const Index = () => {
+  const [plotData, setPlotData] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  // Function to handle form submission, it should send the data to the backend
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("Form submitted");
+  
+    // Create an instance of FormData
+    const formData = new FormData();
+  
+    // Append the files to formData. Since you allow multiple files, append each one.
+    const files = document.getElementById('file-upload').files;
+    for (let i = 0; i < files.length; i++) {
+      formData.append('files', files[i]);
+    }
+  
+    // Append other form data
+    formData.append('species', document.getElementById('species-select').value);
+    formData.append('sequenceLength', document.getElementById('sequence-length').value);
+    formData.append('binSize', document.getElementById('bin-size').value);
+  
+    // Use Fetch API to send the formData to backend
+    try {
+      setLoading(true);
+      const response = await fetch('https://6nzxdwcc-5000.usw2.devtunnels.ms/predict-hic', {
+        method: 'POST',
+        credentials: 'include',
+        body: formData, // No need to set Content-Type header when using FormData
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      // Handle response from the backend
+      const result = await response.json();
+      setLoading(false);
+      setPlotData(result);
+      console.log("Response from the backend:", result);
+    } catch (error) {
+      setLoading(false);
+      console.error("Failed to send form data:", error);
+    }
+  };
+
   // Function to handle file upload, it should send the file to the backend
   const handleFileUpload = (event) => {
     // TODO: Implement file upload logic
@@ -10,28 +59,23 @@ const Index = () => {
     // You would typically send the file to your Flask backend here
   };
 
-  // Function to handle form submission, it should send the data to the backend
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // TODO: Implement form submission logic
-    console.log("Form submitted");
-    // You would typically send the form data to your Flask backend here
-  };
-
   return (
     <VStack height="100vh" padding={4} alignItems="flex-start">
-      <Heading as="h1" size="xl" paddingBottom={2}>
+      <Flex alignItems="flex-end">
+        <Image src="../../public/akita_log.jpeg" alt="Akita Logo" boxSize="60px" marginRight={2} />
+        <Heading as="h1" size="lg">
         Akita
-      </Heading>
-      <Box borderBottom="1px" borderColor="gray.200" width="100%" paddingBottom={4}></Box>
+        </Heading>
+      </Flex>
+      <Box borderBottom="1px" borderColor="gray.200" width="100%"></Box>
       <Flex direction="row" width="100%">
-        <VStack width="30%" borderRight="1px" borderColor="gray.200" paddingRight={4}>
+        <VStack width="30%" borderRight="1px" borderColor="gray.200" paddingRight={4} marginTop={7}>
           <Heading as="h3" size="lg" paddingBottom={2}>
             DNA Sequence Input
           </Heading>
           <FormControl as="form" onSubmit={handleSubmit}>
             <FormLabel htmlFor="file-upload">Upload DNA Sequence File</FormLabel>
-            <Input type="file" id="file-upload" accept=".txt" onChange={handleFileUpload} mb={4} />
+            <Input type="file" id="file-upload" accept=".txt" onChange={handleFileUpload} multiple mb={4} />
             <FormLabel htmlFor="species-select">Select Species</FormLabel>
             <Select id="species-select" placeholder="Select species" mb={4}>
               {/* Add species options here */}
@@ -50,19 +94,27 @@ const Index = () => {
             </Select>
             <Button leftIcon={<FaUpload />} colorScheme="blue" type="submit">
               Submit
+              
             </Button>
           </FormControl>
         </VStack>
-        <Box width="70%" paddingLeft={4}>
-          <Heading as="h3" size="lg" paddingBottom={4}>
-            Hi-C Map Plot
+        <Box width="70%" paddingLeft={4} marginTop={7}>
+          <Heading as="h3" size="lg" paddingBottom={4}> 
+            Hi-C Map
           </Heading>
           <Text>
-            {/* This is where you would display the plotly plot.
-                  Since we can't actually integrate with Plotly here,
-                  let's just assume the plot would go here. */}
-            <Box border="1px" borderColor="gray.200" borderRadius="md" p={4} minHeight="200px">
-              (Hi-C Map Plot will be displayed here)
+            {/* This is where you would display the plotly plot. */}
+            <Box border="1px" borderColor="gray.200" borderRadius="md" p={4} minHeight="400px">
+              {loading ? (
+              <Spinner size="xl" /> // Show the spinner when loading is true
+              ) : (
+                plotData && (
+                  <Plot
+                    data={plotData.data}
+                    layout={plotData.layout}
+                  />
+                ))
+              }
             </Box>
           </Text>
         </Box>
@@ -72,3 +124,5 @@ const Index = () => {
 };
 
 export default Index;
+
+
