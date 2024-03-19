de9e05ae8b9a6d8c3c9f75c0b08bac40e0c29f62
Yaoyx <yxiaoy@umich.edu>
2024-03-19 16:02:16 -0700

    comment out

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: "8080",
-  },
+  // server: {
+  //   host: "0.0.0.0",
+  //   port: "8080",
+  // },
   plugins: [react()],
   base: '/Akita_web/', // Set the base to match your repository path
 });
